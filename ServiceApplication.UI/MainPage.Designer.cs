ebb1d051a6d57f56567121d3b56dff087b31edc9
FurkanAktay <yfaktay@gmail.com>
2023-04-15 23:43:00 +0300

    Some bugs are fixed.

--- ServiceApplication.UI/MainPage.Designer.cs
+++ ServiceApplication.UI/MainPage.Designer.cs
@@ -38,8 +38,6 @@
             this.btnMinimize = new System.Windows.Forms.Button();
             this.btnMaximize = new System.Windows.Forms.Button();
             this.btnClose = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.pnlChildForms.SuspendLayout();
             this.pnlMainPageMenu.SuspendLayout();
             this.pnlMainPageLogo.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pbxLogo)).BeginInit();
@@ -47,7 +45,6 @@
             // 
             // pnlChildForms
             // 
-            this.pnlChildForms.Controls.Add(this.button1);
             this.pnlChildForms.Location = new System.Drawing.Point(166, 59);
             this.pnlChildForms.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pnlChildForms.Name = "pnlChildForms";
@@ -162,15 +159,6 @@
             this.btnClose.UseVisualStyleBackColor = false;
             this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(125, 63);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
             // MainPage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -188,7 +176,6 @@
             this.Name = "MainPage";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Service Application";
-            this.pnlChildForms.ResumeLayout(false);
             this.pnlMainPageMenu.ResumeLayout(false);
             this.pnlMainPageLogo.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pbxLogo)).EndInit();
@@ -207,7 +194,6 @@
         private System.Windows.Forms.Button btnMinimize;
         private System.Windows.Forms.Button btnMaximize;
         private System.Windows.Forms.Button btnClose;
-        private System.Windows.Forms.Button button1;
     }
 }
 
