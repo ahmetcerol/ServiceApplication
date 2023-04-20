9284e4ebda3a64138403d137a0d1d308b41ae526
FurkanAktay <yfaktay@gmail.com>
2023-04-21 00:42:33 +0300

    Remade data and SQL codes.

--- ServiceApplication.DataAccess/Concrete/SQL/ServiceDal.cs
+++ ServiceApplication.DataAccess/Concrete/SQL/ServiceDal.cs
@@ -14,7 +14,7 @@ namespace ServiceApplication.DataAccess.Concrete.SQL
     public class ServiceDal:IServiceDal
     {
         string connectionString = "Data Source=DORUKPC5;Initial Catalog=master;User ID=sa;Password=sapass;Persist Security Info =False"; // Bağlantı dizesi
-        string databaseName = "MyVdsDatabase"; // VDS veritabanı adı
+        string databaseName = "ServiceAPP"; // VDS veritabanı adı
 
         public void CheckAndCreate() {
             string tableName = "ServiceApp";
@@ -25,17 +25,17 @@ namespace ServiceApplication.DataAccess.Concrete.SQL
         {
             if (CheckDatabaseExists(connectionString, databaseName))
             {
-                Console.WriteLine("VDS veritabanı zaten mevcut.");
+                Console.WriteLine("ServiceAPP veritabanı zaten mevcut.");
             }
             else
             {
                 if (CreateDatabase(connectionString, databaseName))
                 {
-                    Console.WriteLine("VDS veritabanı başarıyla oluşturuldu.");
+                    Console.WriteLine("ServiceAPP veritabanı başarıyla oluşturuldu.");
                 }
                 else
                 {
-                    Console.WriteLine("VDS veritabanı oluşturma hatası.");
+                    Console.WriteLine("ServiceAPP veritabanı oluşturma hatası.");
                 }
             }
         }
@@ -142,24 +142,22 @@ namespace ServiceApplication.DataAccess.Concrete.SQL
 
                     // Tablo oluşturma komutunu hazırla
                     string createTableQuery = $"USE {databaseName}; CREATE TABLE {tableName} (SERVICE_ID INT PRIMARY KEY," +
-                        $"CUSTOMER_NAME NVARCHAR(255)," +
-                        $"CUSTOMER_SURNAME NVARCHAR(255)," +
-                        $"CUSTOMER_ADDRESS NVARCHAR(255), " +
-                        $"CUSTOMER_TELEPHONE INT, " +
-                        $"CUSTOMER_EMAİL NVARCHAR(255), " +
-                        $"PERSON_OF_CONTACT NVARCHAR(255)," +
-                        $"DESC_PROPERTY NVARCHAR(MAX)," +
-                        $"SPECİAL_NOTE NVARCHAR(MAX), " +
-                        $"ACTUALLY_PERFORMED INT," +
-                        $"JOB_KİND NVARCHAR(255), " +
-                        $"DATE_SERTAKEPLACE DATETIME, " +
-                        $"DATE_SERTOOKPLACE DATETIME," +
-                        $"DATE_TWOWEEKS DATETIME,  " +
-                        $"CHARGE_BEING INT," +
-                        $"CHARGE_DONE INT, " +
-                        $"PAID_ALONG INT, " +
-                        $"BALANCE INT," +
-                        $"ResimData VARBINARY(MAX) NULL)";
+                         $"CUSTOMER_NAME NVARCHAR(255)," +
+                         $"CUSTOMER_SURNAME NVARCHAR(255)," +
+                         $"CUSTOMER_TELEPHONE INT, " +
+                         $"CUSTOMER_EMAIL NVARCHAR(255), " +
+                         $"CUSTOMER_ADRESS NVARCHAR(255), " +
+                         $"PERSON_OF_CONTACT NVARCHAR(255)," +
+                         $"DETAILS_OF_PROPERTY NVARCHAR(MAX)," +
+                         $"PRICE_OF_DAY INT, " +
+                         $"JOB_KIND NVARCHAR(255), " +
+                         $"START_DATE DATE, " +
+                         $"CUT_DATE DATE," +
+                         $"START_ACT_DATE DATE,  " +
+                         $"DATE_OF_SER_ACT DATE,  " +
+                         $"SERVICE_CHARGE INT," +
+                         $"SPECIAL_NOTE NVARCHAR(MAX), " +
+                         $"ResimData VARBINARY(MAX) NULL)";
 
                     // Komutu SQL Server'a gönder
                     using (SqlCommand command = new SqlCommand(createTableQuery, connection))
@@ -189,21 +187,19 @@ namespace ServiceApplication.DataAccess.Concrete.SQL
                     SERVICE_ID = Convert.ToInt32(reader["SERVICE_ID"]),
                     CUSTOMER_NAME = Convert.ToString(reader["CUSTOMER_NAME"]),
                     CUSTOMER_SURNAME= Convert.ToString(reader["CUSTOMER_SURNAME"]),
-                    CUSTOMER_ADDRESS = Convert.ToString(reader["CUSTOMER_ADDRESS"]),
                     CUSTOMER_TELEPHONE = Convert.ToInt32(reader["CUSTOMER_TELEPHONE"]),
-                    CUSTOMER_EMAİL = Convert.ToString(reader["CUSTOMER_EMAİL"]),
+                    CUSTOMER_EMAIL = Convert.ToString(reader["CUSTOMER_EMAIL"]),
+                    CUSTOMER_ADRESS = Convert.ToString(reader["CUSTOMER_ADRESS"]),
                     PERSON_OF_CONTACT = Convert.ToString(reader["PERSON_OF_CONTACT"]),
-                    DESC_PROPERTY = Convert.ToString(reader["DESC_PROPERTY"]),
-                    SPECİAL_NOTE = Convert.ToString(reader["SPECİAL_NOTE"]),
-                    ACTUALLY_PERFORMED = Convert.ToInt32(reader["ACTUALLY_PERFORMED"]),
-                    JOB_KİND = Convert.ToString(reader["JOB_KİND"]),
-                    DATE_SERTAKEPLACE = Convert.ToDateTime(reader["DATE_SERTAKEPLACE"]),
-                    DATE_SERTOOKPLACE = Convert.ToDateTime(reader["DATE_SERTOOKPLACE"]),
-                    DATE_TWOWEEKS = Convert.ToDateTime(reader["DATE_TWOWEEKS"]),
-                    CHARGE_BEING = Convert.ToInt32(reader["CHARGE_BEING"]),
-                    CHARGE_DONE = Convert.ToInt32(reader["CHARGE_DONE"]),
-                    PAID_ALONG = Convert.ToInt32(reader["PAID_ALONG"]),
-                    BALANCE = Convert.ToInt32(reader["BALANCE"]),
+                    DETAILS_OF_PROPERTY = Convert.ToString(reader["DETAILS_OF_PROPERTY"]),
+                    PRICE_OF_DAY = Convert.ToInt32(reader["PRICE_OF_DAY"]),
+                    JOB_KIND = Convert.ToString(reader["JOB_KIND"]),
+                    START_DATE = Convert.ToDateTime(reader["START_DATE"]),
+                    CUT_DATE = Convert.ToDateTime(reader["CUT_DATE"]),
+                    START_ACT_DATE = Convert.ToDateTime(reader["START_ACT_DATE"]),
+                    DATE_OF_SER_ACT = Convert.ToDateTime(reader["DATE_OF_SER_ACT"]),
+                    SERVICE_CHARGE = Convert.ToInt32(reader["SERVICE_CHARGE"]),
+                    SPECIAL_NOTE = Convert.ToString(reader["SPECIAL_NOTE"]),
                     SELECTED_IMAGE_DATA= (byte[])reader["SELECTED_IMAGE_DATA"],
             };
                 serviceInfo.Add(service);
@@ -220,26 +216,24 @@ namespace ServiceApplication.DataAccess.Concrete.SQL
             CheckAndCreate();
             SqlConnection aconnection = new SqlConnection("...");
             SqlCommand command = new SqlCommand(
-                "INSERT INTO YourTableName VALUES (@CUSTOMER_NAME,@CUSTOMER_SURNAME, @CUSTOMER_ADDRESS, @CUSTOMER_TELEPHONE, @CUSTOMER_EMAİL, @PERSON_OF_CONTACT, @DESC_PROPERTY, @SPECİAL_NOTE, " +
-                "@ACTUALLY_PERFORMED, @JOB_KİND, @DATE_SERTAKEPLACE, @DATE_SERTOOKPLACE, @DATE_TWOWEEKS, @CHARGE_BEING, @CHARGE_DONE, @PAID_ALONG, @BALANCE,@SELECTED_IMAGE_DATA", aconnection);
+                "INSERT INTO YourTableName VALUES (@CUSTOMER_NAME,@CUSTOMER_SURNAME,@CUSTOMER_TELEPHONE,   @CUSTOMER_EMAIL,@CUSTOMER_ADRESS, @PERSON_OF_CONTACT, @DETAILS_OF_PROPERTY, @PRICE_OF_DAY, " +
+                "@JOB_KIND, @START_DATE, @CUT_DATE, @START_ACT_DATE, @DATE_OF_SER_ACT, @SERVICE_CHARGE, @SPECIAL_NOTE,@SELECTED_IMAGE_DATA", aconnection);
 
             command.Parameters.AddWithValue("@CUSTOMER_NAME", service.CUSTOMER_NAME);
             command.Parameters.AddWithValue("@CUSTOMER_SURNAME", service.CUSTOMER_SURNAME);
-            command.Parameters.AddWithValue("@CUSTOMER_ADDRESS", service.CUSTOMER_ADDRESS);
             command.Parameters.AddWithValue("@CUSTOMER_TELEPHONE", service.CUSTOMER_TELEPHONE);
-            command.Parameters.AddWithValue("@CUSTOMER_EMAİL", service.CUSTOMER_EMAİL);
+            command.Parameters.AddWithValue("@CUSTOMER_EMAIL", service.CUSTOMER_EMAIL);
+            command.Parameters.AddWithValue("@CUSTOMER_ADRESS", service.CUSTOMER_ADRESS);            
             command.Parameters.AddWithValue("@PERSON_OF_CONTACT", service.PERSON_OF_CONTACT);
-            command.Parameters.AddWithValue("@DESC_PROPERTY", service.DESC_PROPERTY);
-            command.Parameters.AddWithValue("@SPECİAL_NOTE", service.SPECİAL_NOTE);
-            command.Parameters.AddWithValue("@ACTUALLY_PERFORMED", service.ACTUALLY_PERFORMED);
-            command.Parameters.AddWithValue("@JOB_KİND", service.JOB_KİND);
-            command.Parameters.AddWithValue("@DATE_SERTAKEPLACE", service.DATE_SERTAKEPLACE);
-            command.Parameters.AddWithValue("@DATE_SERTOOKPLACE", service.DATE_SERTOOKPLACE);
-            command.Parameters.AddWithValue("@DATE_TWOWEEKS", service.DATE_TWOWEEKS);
-            command.Parameters.AddWithValue("@CHARGE_BEING", service.CHARGE_BEING);
-            command.Parameters.AddWithValue("@CHARGE_DONE", service.CHARGE_DONE);
-            command.Parameters.AddWithValue("@PAID_ALONG", service.PAID_ALONG);
-            command.Parameters.AddWithValue("@BALANCE", service.BALANCE);
+            command.Parameters.AddWithValue("@DETAILS_OF_PROPERTY", service.DETAILS_OF_PROPERTY);
+            command.Parameters.AddWithValue("@PRICE_OF_DAY", service.PRICE_OF_DAY);
+            command.Parameters.AddWithValue("@JOB_KIND", service.JOB_KIND);
+            command.Parameters.AddWithValue("@START_DATE", service.START_DATE);
+            command.Parameters.AddWithValue("@CUT_DATE", service.CUT_DATE);
+            command.Parameters.AddWithValue("@START_ACT_DATE", service.START_ACT_DATE);
+            command.Parameters.AddWithValue("@DATE_OF_SER_ACT", service.DATE_OF_SER_ACT);
+            command.Parameters.AddWithValue("@SERVICE_CHARGE", service.SERVICE_CHARGE);
+            command.Parameters.AddWithValue("@SPECIAL_NOTE", service.SPECIAL_NOTE);
             command.Parameters.AddWithValue("@SELECTED_IMAGE_DATA", service.SELECTED_IMAGE_DATA);
             command.ExecuteNonQuery();
             aconnection.Close();
--- ServiceApplication.Entities/Concrete/Service.cs
+++ ServiceApplication.Entities/Concrete/Service.cs
@@ -12,21 +12,19 @@ namespace ServiceApplication.Entities.Concrete
         public int SERVICE_ID { get; set; }
         public string CUSTOMER_NAME { get; set;}
         public string CUSTOMER_SURNAME { get; set; }
-        public string CUSTOMER_ADDRESS { get; set; }
-        public int CUSTOMER_TELEPHONE { get; set; } 
-        public string CUSTOMER_EMAİL { get; set; }
+        public int CUSTOMER_TELEPHONE { get; set; }
+        public string CUSTOMER_EMAIL { get; set; }
+        public string CUSTOMER_ADRESS { get; set; }
         public string PERSON_OF_CONTACT { get; set; }
-        public string DESC_PROPERTY { get; set; }
-        public string SPECİAL_NOTE { get; set; }
-        public int ACTUALLY_PERFORMED { get; set; }
-        public string JOB_KİND { get; set; }
-        public DateTime DATE_SERTAKEPLACE { get; set; }
-        public DateTime DATE_SERTOOKPLACE { get; set; } 
-        public DateTime DATE_TWOWEEKS { get; set; }
-        public int CHARGE_BEING { get; set; }   
-        public int CHARGE_DONE { get; set; }
-        public int PAID_ALONG { get; set; }
-        public int BALANCE { get; set; }
+        public string DETAILS_OF_PROPERTY { get; set; }
+        public int PRICE_OF_DAY { get; set; }
+        public string JOB_KIND { get; set; }
+        public DateTime START_DATE { get; set; }
+        public DateTime CUT_DATE { get; set; } 
+        public DateTime START_ACT_DATE { get; set; }
+        public DateTime DATE_OF_SER_ACT { get; set; }   
+        public int SERVICE_CHARGE { get; set; }
+        public string SPECIAL_NOTE { get; set; }
         public byte[] SELECTED_IMAGE_DATA { get; set; } 
 
     }
